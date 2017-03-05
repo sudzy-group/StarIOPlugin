0efc47c62a401119c2475b745a50b21eb40c2769
Hilary Barr <hilaryrosebarr@gmail.com>
2017-03-04 20:08:43 -0500

    Fix commands string

--- src/ios/StarIOPlugin.m
+++ src/ios/StarIOPlugin.m
@@ -222,8 +222,9 @@ static NSString *dataCallbackId = nil;
             port = [_starIoExtManager port];
         }
 
+        [commands appendData:[content dataUsingEncoding:NSASCIIStringEncoding]];
 
-        [commands appendBytes:content length:sizeof(content - 1];   
+        // [commands appendBytes:content length:sizeof(content - 1];   
 
         // [commands appendBytes:"\x1b\x1d\x61\x01" length:sizeof("\x1b\x1d\x61\x01") - 1];    // Alignment (Center)
 
